eadb63f35f56a0b4cf4976702d21f34bc0f108c9
zzinue <zzinue@gmail.com>
2024-11-25 21:48:18 -0600

    adding fonts, styles anddnd title

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold  ">
+        Welcome to Awwards
+      </h1>
+    </main>
+  );
+};
+
+export default App;
