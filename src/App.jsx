fb5439d039ec3082cb084b0cf0d846ebd0fdbcf6
zzinue <zzinue@gmail.com>
2024-11-29 21:31:03 -0600

    changing website name

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <main>
       <h1 className="text-5xl text-orange-500 font-bold  ">
-        Welcome to Awwards
+        Welcome to Awwards Website
       </h1>
     </main>
   );
